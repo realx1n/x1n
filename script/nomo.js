fd8439675d5e87e2a869d0abf4761390fbc8989d
BigBroX <lorexu1205@gmail.com>
2021-04-27 17:50:51 +0800

    Create nomo.js

---
+++ script/nomo.js
@@ -0,0 +1,4 @@
+hostname = nomo.dafork.com
+
+# NOMO
+^https:\/\/nomo\.dafork\.com\/api\/v2\/iap\/ios_product_list url script-response-body https://raw.githubusercontent.com/zxd2008117/QX/master/NOMO.js
